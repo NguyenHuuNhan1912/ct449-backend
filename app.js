79c30bf7e79752e1131a6706cc800019ad27fad8
NguyenHuuNhan1912 <nhanpi00123456789@gmail.com>
2022-09-14 14:59:09 +0700

    Err error

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const express = require("express");
 // Sử dụng thư viện cors để trao đổi dữ liệu chéo từ client đến server
 const cors = require("cors");
 
+// Import file api error
+const ApiError = require("./app/api-error");
 // Gọi đến thư viện express và sử dụng nó
 const app = express();
 
@@ -23,20 +25,24 @@ app.get("/", (req, res) => {
         message: "Welcome to contact book application."
     });
 })
+// Các route quản lý liên hệ sẽ được dùng khi đường dẫn bắt đầu là /api/contacts.
 app.use("/api/contacts", contactsRouter);
+// handle 404 response
+
 // handle 404 response
 app.use((req, res, next) => {
     // Code ở đây sẽ chạy khi không có route được định nghĩa nào
     // khớp với yêu cầu. Gọi next() để chuyển sang middleware xử lý lỗi
     return next(new ApiError(404, "Resource not found"));
 });
+
 // define error-handling middleware last, after other app.use() and routes calls
 app.use((err, req, res, next) => {
     // Middleware xử lý lỗi tập trung.
     // Trong các đoạn code xử lý ở các route, gọi next(error)
     // sẽ chuyển về middleware xử lý lỗi này
-    return res.status(error.statusCode || 500).json({
-        message: error.message || "Internal Server Error",
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
     });
 });
 module.exports = app;
